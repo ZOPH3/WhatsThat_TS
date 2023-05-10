0101062bd05cb1251d42e25cfec0efdae4cd2f78
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-05-10 21:40:00 +0100

    Messages controller result mess removed #1

--- src/controllers/message.controller.ts
+++ src/controllers/message.controller.ts
@@ -4,44 +4,23 @@ import UrlBuilder from "../util/url.builder";
 
 // https://github.com/ZJav1310/WhatsThat_TS/issues/1
 class MessageController {
-  
   static async sendMessage(chat_id: number, message: string) {
-    const myHeaders = await AuthHeader();
-
+    // const myHeaders = await AuthHeader();
     const requestOptions: RequestInit = {
       method: "POST",
-      headers: myHeaders,
+      headers: await AuthHeader(),
       body: JSON.stringify({ message: message }),
-      redirect: "follow",
     };
 
     return fetch(UrlBuilder.sendMessage(chat_id), requestOptions)
-      .then((response) => {
-        return {
-          status: true,
-          message: `Sent new message in chat ${chat_id}.`,
-          result: response,
-        };
-      })
-      .catch((error) => {
-        return {
-          status: false,
-          message: `Unable to send message in chat ${chat_id}...`,
-          result: "",
-          error: error,
-        };
-      });
+      .then((response) => response)
+      .catch((error) =>
+        console.log("Error caught while sending message: ", error)
+      );
   }
 
   static async deleteMessage(chat_id: number, message_id: number) {
-    const myHeaders = new Headers();
-    const value = await AuthService.getToken();
-
-    if (value.status) {
-      myHeaders.append("X-Authorization", value.result);
-    }
-
-    myHeaders.append("Content-Type", "application/json");
+    const myHeaders = await AuthHeader();
 
     const requestOptions: RequestInit = {
       method: "DELETE",
@@ -50,21 +29,10 @@ class MessageController {
     };
 
     return fetch(UrlBuilder.deleteMessage(chat_id, message_id), requestOptions)
-      .then((response) => {
-        return {
-          status: true,
-          message: `Deleted message ${message_id} in chat ${chat_id}.`,
-          result: response,
-        };
-      })
-      .catch((error) => {
-        return {
-          status: false,
-          message: `Unable to delete message ${message_id} from chat ${chat_id}...`,
-          result: "",
-          error: error,
-        };
-      });
+      .then((response) => response)
+      .catch((error) =>
+        console.log("Error caught while deleting message: ", error)
+      );
   }
 
   static async updateMessage(
@@ -72,14 +40,7 @@ class MessageController {
     message_id: number,
     message: string
   ) {
-    const myHeaders = new Headers();
-    const value = await AuthService.getToken();
-
-    if (value.status) {
-      myHeaders.append("X-Authorization", value.result);
-    }
-
-    myHeaders.append("Content-Type", "application/json");
+    const myHeaders = await AuthHeader();
 
     const requestOptions: RequestInit = {
       method: "PATCH",
@@ -90,21 +51,10 @@ class MessageController {
 
     return fetch(UrlBuilder.updateMessage(chat_id, message_id), requestOptions)
       .then((response) => response.json())
-      .then((response) => {
-        return {
-          status: true,
-          message: `Updated message ${message_id} in chat ${chat_id}.`,
-          result: response,
-        };
-      })
-      .catch((error) => {
-        return {
-          status: false,
-          message: `Unable to update message ${message_id} in chat ${chat_id}...`,
-          result: "",
-          error: error,
-        };
-      });
+      .then((response) => response)
+      .catch((error) =>
+        console.log("Error caught while updating message: ", error)
+      );
   }
 }
 

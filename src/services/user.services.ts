eb1ff0dd6ff06bbccf3a1112c2efa6227aa8ff06
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-05-11 19:38:21 +0100

    Simplfied services

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,51 +1,38 @@
 import UserController from "../controllers/user.controller";
 import UserType from "../util/types/user.type";
-import { logOutput, logType } from "../util/logger.util";
 
 class UserService {
   public static async login(email: string, password: string) {
-    const data = await UserController.login(email, password);
-    const type = data.status ? logType.success : logType.error;
-    logOutput(type, data.message);
-    return data;
+    const response = await UserController.login(email, password) ?? undefined;
+    return response;
   }
 
   public static async logout() {
-    const data = await UserController.logout();
-    const type = data.status ? logType.success : logType.error;
-    logOutput(type, data.message);
-    return data;
+    const response = await UserController.logout() ?? undefined;
+    return response;
   }
+
   public static async register(
     first_name: string,
     last_name: string,
     email: string,
     password: string
   ) {
-    const data = await UserController.register(
-      first_name,
-      last_name,
-      email,
-      password
-    );
-    const type = data.status ? logType.success : logType.error;
-    logOutput(type, data.message);
-    return data;
+    const response = await UserController.register(first_name, last_name, email, password) ?? undefined;
+    return response;
   }
+
   public static async getUserInfo(user_id: number) {
-    const data = await UserController.getUserInfo(user_id);
-    const type = data.status ? logType.success : logType.error;
-    logOutput(type, data.message);
-    return data;
+    const response = await UserController.getUserInfo(user_id) ?? undefined;
+    return response;
   }
+
   public static async updateUserInfo(
     user_id: number,
     payload: Partial<UserType>
   ) {
-    const data = await UserController.updateUserInfo(user_id, payload);
-    const type = data.status ? logType.success : logType.error;
-    logOutput(type, data.message);
-    return data;
+    const response = await UserController.updateUserInfo(user_id, payload) ?? undefined;
+    return response;
   }
 }
 

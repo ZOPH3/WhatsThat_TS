e81b20a124859904ff41c5984fe874fa298bc33c
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-05-31 15:56:42 +0100

    Global Context

--- App.tsx
+++ App.tsx
@@ -3,32 +3,29 @@ import StackNavigator from './src/navigation/main.stack.navigator';
 
 import { AuthContext } from './src/context/auth.context';
 import { UserContext } from './src/context/user.context';
+import { GlobalProvider } from './src/context/GlobalContext';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
   const [user, setUser] = React.useState({
     user_id: -1,
-    first_name: "",
-    last_name: "",
-    email: "",
+    first_name: '',
+    last_name: '',
+    email: '',
   });
 
-  const value = React.useMemo(
-    () => ({ isLoggedIn, setIsLoggedIn }),
-    [isLoggedIn]
-  );
+  const value = React.useMemo(() => ({ isLoggedIn, setIsLoggedIn }), [isLoggedIn]);
 
-  const userValue = React.useMemo(
-    () => ({ user, setUser }),
-    [user]
-  );
+  const userValue = React.useMemo(() => ({ user, setUser }), [user]);
 
   return (
+    <GlobalProvider>
       <AuthContext.Provider value={value}>
         <UserContext.Provider value={userValue}>
           <StackNavigator />
         </UserContext.Provider>
       </AuthContext.Provider>
+    </GlobalProvider>
   );
 }
 export default App;

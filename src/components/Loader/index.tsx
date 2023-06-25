1a6a805db9ca6788186d4a230eab7968edb5c141
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-06-25 04:04:26 +0100

    Componentrender component template

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
 import { View } from 'react-native';
 
-interface ILoaderProps {
-    onLoading: any;
-    onSuccess: any;
-    onEmpty: any;
-    onError: any;
+interface IComponentState {
+  name: string; // Name of the component
+  state?: 'loading' | 'success' | 'empty' | 'error';
 }
-const Loader = (props: ILoaderProps) => {
+
+interface IComponents {
+    onLoading?: any;
+    onSuccess?: any;
+    onEmpty?: any;
+    onError?: any;
+}
+
+// Template to deal with state of component.
+const ComponentRender = (props: IComponentState & IComponents) => {
+  const { name, state, onLoading, onSuccess, onEmpty, onError } = props;
+
+  if (state === 'loading') {
+    return onLoading? onLoading : <View></View>;
+  }
+
+  if (state === 'success') {
+    return onSuccess? onSuccess : <View></View>;
+  }
+
+  if (state === 'empty') {
+    return onEmpty? onEmpty : <View></View>;
+  }
+
+  if (state === 'error') {
+    return onError? onError : <View></View>;
+  }
+
   return <View></View>;
 };
+
+export default ComponentRender;
+
+/**
+ * Usage:
+ *  <ComponentRender
+ *    name="ComponentName"
+ *    state="loading"
+ *    onLoading={<View><Text>Loading...</Text></View>}
+ *    onSuccess={<View><Text>Success</Text></View>}
+ *    onEmpty={<View><Text>Empty</Text></View>}
+ *    onError={<View><Text>Error</Text></View>}
+ * />
+ * 
+ * 
+ */

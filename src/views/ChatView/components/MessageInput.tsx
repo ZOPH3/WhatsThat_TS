56b3a3325fc620984ded7cd7f1b50ef33c7289d6
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-07-01 04:12:50 +0100

    Added FAB button to message send

--- src/views/ChatView/components/MessageInput.tsx
+++ src/views/ChatView/components/MessageInput.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { TextInput } from 'react-native-paper';
+import { FAB, Portal, TextInput } from 'react-native-paper';
 import { View } from 'react-native';
 
 import styles from '../../../styles/GlobalStyle';
-
 import ButtonComponent from '../../../components/Button';
 
-function MessageInput({ onSend }) {
+function MessageInput({ onSend, onDraft }) {
   const [inputValue, setInputValue] = useState('');
+
+  const [state, setState] = React.useState({ open: false });
+  const onStateChange = ({ open }) => setState({ open });
+  const { open } = state;
 
   const handleInputChange = (text) => {
     setInputValue(text);
@@ -18,8 +21,15 @@ function MessageInput({ onSend }) {
     setInputValue('');
   };
 
+  const handleDraftButtonPress = () => {
+    onDraft(inputValue);
+    setInputValue('');
+  };
+
   return (
-    <View style={styles.bottomView}>
+    <View
+      style={{ ...styles.bottomView, width: '80%', padding: 15, margin: 0, paddingHorizontal: 10 }}
+    >
       <TextInput
         mode="outlined"
         value={inputValue}
@@ -31,12 +41,38 @@ function MessageInput({ onSend }) {
         onChangeText={handleInputChange}
         style={{ flex: 10, padding: 6 }}
       />
-      <ButtonComponent
+      {/* <ButtonComponent
         mode="contained"
         style={styles.btnAdd}
         title="Send"
         onPress={handleSendButtonPress}
-      />
+      /> */}
+      <Portal>
+        <FAB.Group
+          style={{ position: 'absolute', margin: 5, bottom: 35 }}
+          open={open}
+          visible
+          toggleStackOnLongPress
+          icon={open ? 'dots-vertical' : 'send'}
+          actions={[
+            {
+              icon: 'archive',
+              label: 'View Drafts',
+              onPress: () => console.log('Pressed notifications'),
+            },
+            {
+              icon: 'file-document-edit',
+              label: 'Save to drafts',
+              onPress: () => console.log('Save to drafts'),
+            },
+          ]}
+          onStateChange={onStateChange}
+          onPress={() => {
+            console.log('Pressed send');
+            handleSendButtonPress();
+          }}
+        />
+      </Portal>
     </View>
   );
 }

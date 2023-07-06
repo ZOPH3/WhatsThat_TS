fcc6501666a217d42c8f19a50ef54a6e17cd9087
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-07-06 19:21:59 +0100

    Removed old type

--- src/lib/types/TStorageKeys.ts
+++
@@ -1,6 +0,0 @@
-export enum TStorageKeys {
-    AuthToken = '@AuthToken',
-    SavedUser = '@SavedUser',
-    SavedChatSummaries = '@SavedChatSummaries',
-    SavedMessageDraft = '@SavedMessageDraft'
-  }

996989835a64770587eda64894e9211947505d79
ZJAV1310 <56839787+ZJav1310@users.noreply.github.com>
2023-07-08 02:42:29 +0100

    Draft when sent is deleted from state, chats user not longer is in also deleted.

--- src/lib/controller/DraftController.tsx
+++ src/lib/controller/DraftController.tsx
@@ -6,19 +6,26 @@ import { TDraftMessage } from '../context/services/types';
 import MessageServices from '../services/MessageServices';
 
 import { pollingLog } from '../util/LoggerUtil';
+import { useChat } from '../context/chats';
 
 const DraftController = () => {
   const { apiCaller } = useApi();
   const s = useService();
+  const c = useChat();
   const m = MessageServices(apiCaller);
 
   const _send = (draft: TDraftMessage) => {
     if (!draft || !draft.message || !draft.message.message) return;
+    const chat = c.chatSummaryList.find((c) => c.chat_id === draft.chat_id);
+    if (!chat) {
+      s.dispatcher.deleteDraftMessage(draft.draft_id);
+      return;
+    }
     m.sendMessage(draft.chat_id, draft.message?.message)
       .then((res) => {
         if (!res) return;
         pollingLog.info('Draft Sent: ', res);
-        s.dispatcher.deleteDraft(draft.draft_id);
+        s.dispatcher.deleteDraftMessage(draft.draft_id);
       })
       .catch((err) => {
         pollingLog.warn('Unable to send draft...');
@@ -33,7 +40,7 @@ const DraftController = () => {
       data.forEach(async (draft) => {
         if (!draft.sent) {
           if (draft.scheduled <= Date.now()) {
-            console.log(draft);
+            // console.log(draft);
             _send(draft);
           }
         }
